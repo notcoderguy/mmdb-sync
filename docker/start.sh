82db865ed26af133d8ef1ef078d78a0c7249a1db
Vasu Grover <notcoderguy@gmail.com>
2025-06-25 14:07:34 +0000

    feat: add database touch artisan command

--- docker/start.sh
+++ docker/start.sh
@@ -4,8 +4,9 @@
 # Run migrations and seed the database if needed (optional, can also be done at runtime)
 if [ ! -f storage/db/database.sqlite ]; then
     echo "Database file not found. Creating a new one..."
-    sudo touch storage/db/database.sqlite
-    sudo chown -R www-data:www-data storage/db
+    # sudo touch storage/db/database.sqlite
+    # sudo chown -R www-data:www-data storage/db
+    php artisan app:db:touch
     php artisan migrate:fresh --force
 fi
 
